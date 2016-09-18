e10952f816d4bcf9bafa9060bdab7e9f7291c6f0
Marry_Krotter <mashandrey20@gmail.com>
2016-09-18 22:17:26 +0800

    Моя первая программа ура

---
+++ 1 Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,16 @@
+// Hello World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+void main()
+{
+	setlocale (LC_ALL, "RUS");
+	cout << "Hello World - это скучно,поэтому слава Енотам!" << endl;
+	system ("pause");
+}
+

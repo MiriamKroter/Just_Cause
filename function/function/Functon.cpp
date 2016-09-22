cae7b6dd811d51227e4753da046241f0c945d682
Marry_Krotter <mashandrey20@gmail.com>
2016-09-22 19:18:34 +0800

    функции

---
+++ function/function/Functon.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+int razn(int a, int b)
+{
+	return a - b;
+}
+int main()
+{   
+	setlocale(LC_ALL, "RUS");
+	int   a = 5, b = 4;
+
+	cout << "Сумма: "<< sum(a, b)<< "\nРазность: "<< razn(a, b) << endl;
+	system("pause");
+}
+

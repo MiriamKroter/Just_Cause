a804b80f20095c9918984be5b0048f58f52ffc18
Marry_Krotter <mashandrey20@gmail.com>
2016-09-25 00:26:50 +0800

    остальной дотокод

--- function/function/Functon.cpp
+++ function/function/Functon.cpp
@@ -25,13 +25,33 @@ int segm(int a, int b)
 		c++;
 	}
 		return c;
-	
-		
 }
+
+int summ(int a)
+{
+	if (a == 0) 
+		return 0;
+	else return  a + summ(a - 1);
+}
+
+int fact(int a) {
+	if (a == 0)
+		return 1;
+	else return a * fact(a - 1);
+}
+
+int fib(int a) {
+	if (a <= 2)
+		return 1;
+	else return fib(a - 1) + fib(a - 2);
+}
+
 void main()
 {
 	setlocale(LC_ALL, "RUS");
 	int  a = 12, b = 4;
-	cout << "Сумма: " << sum(a, b) << "\nРазность: " << razn(a, b) << "\nУмножение: "<< multip(a,b) << "\nДеление: "<< segm (a,b) << endl;
+	cout << "Сумма: " << sum(a, b) << "\nРазность: " << razn(a, b) << "\nУмножение: " << multip(a, b) << endl;
+	cout << "Деление: " << segm(a, b) << "\nСумма чисел от 1 до N (рекурсия): " << summ(a) << endl;
+	cout << "n!+4*F(n)=" << sum(fact(b), multip(4, fib(b))) << endl;
 	system("pause");
 }
